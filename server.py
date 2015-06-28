cff023eb42227b6a08b903894cc7370b68037e75
root <root@localhost.localdomain>
2015-06-28 20:25:42 +0700

    Variable not found test new one

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('hello.jinja', domain=os.environ['SERVER_NAME'])
+    return render_template('hello.jinja')
 
 app.debug = True
 app.run(host='0.0.0.0', port=int(os.environ['PORT']))
